401875ba512b57264b32e54199eb79e51b069d33
unixjang <unixjang@naver.com>
2020-04-14 18:22:50 +0900

    yunhee

--- app/src/main/java/com/example/getscholarship/MainActivity.java
+++ app/src/main/java/com/example/getscholarship/MainActivity.java
@@ -21,6 +21,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button)findViewById(R.id.bt_list);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button2 = (Button)findViewById(R.id.bt_matching);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MatchingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3= (Button) findViewById(R.id.bt_link);
+        button3.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kosaf.go.kr/"));
+                startActivity(intent);
+            }
+        });
+
+        Button button4 = (Button)findViewById(R.id.bt_notification);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button5 = (Button)findViewById(R.id.bt_favorite);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button6 = (Button)findViewById(R.id.bt_mypage);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MypageActivity.class);
+                startActivity(intent);
+            }
+        });
+
         int images[] = {R.drawable.ad1,R.drawable.ad2,R.drawable.ad3,R.drawable.ad4};
         viewFlipper = findViewById(R.id.flipper);
 
