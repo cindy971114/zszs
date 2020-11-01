b27306bb6cb93ad2e7f3362a5050c27ac306b413
GetScholarship <cindy971114@naver.com>
2020-11-01 16:35:52 +0900

    hard

--- app/src/main/java/com/example/getscholarship/NotificationActivity.java
+++ app/src/main/java/com/example/getscholarship/NotificationActivity.java
@@ -20,14 +20,15 @@ public class NotificationActivity extends AppCompatActivity {
 
         noticeListView = (ListView) findViewById(R.id.noticeListView);
         noticeList = new ArrayList<Notice>();
-        noticeList.add(new Notice("후원해주세요","관리자","2020-05-01"));
-        noticeList.add(new Notice("모두에게 알립니다","관리자","2020-05-01"));
-        noticeList.add(new Notice("중요 공지","관리자","2020-05-01"));
-        noticeList.add(new Notice("개인 위생을 철저히","관리자","2020-04-30"));
-        noticeList.add(new Notice("코로나 19 이겨냅시다.","관리자","2020-04-05"));
-        noticeList.add(new Notice("4월 공지","관리자","2020-04-01"));
-        noticeList.add(new Notice("3월 공지","관리자","2020-03-01"));
-        noticeList.add(new Notice("2월 공지","관리자","2020-02-01"));
+        noticeList.add(new Notice("후원해주세요","관리자","2020-11-01"));
+        noticeList.add(new Notice("모두에게 알립니다","관리자","2020-11-01"));
+        noticeList.add(new Notice("중요 공지","관리자","2020-11-01"));
+        noticeList.add(new Notice("11월 공지","관리자","2020-11-01"));
+        noticeList.add(new Notice("개인 위생을 철저히","관리자","2020-10-30"));
+        noticeList.add(new Notice("코로나 19 이겨냅시다.","관리자","2020-10-28"));
+        noticeList.add(new Notice("10월 공지","관리자","2020-10-01"));
+        noticeList.add(new Notice("9월 공지","관리자","2020-09-01"));
+        noticeList.add(new Notice("8월 공지","관리자","2020-08-01"));
         noticeList.add(new Notice("국가장학금 신청하세요","관리자","2020-01-20"));
         noticeList.add(new Notice("1월 공지","관리자","2020-01-10"));
         noticeList.add(new Notice("서비스를 시작합니다.","관리자","2020-01-01"));
